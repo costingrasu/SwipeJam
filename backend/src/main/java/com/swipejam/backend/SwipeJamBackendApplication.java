4f9251109c5fb72720b6081d281fc8c7849dac28
costingrasu <grasucostin@gmail.com>
2026-04-13 15:46:51 +0300

    added backend skeleton

---
+++ backend/src/main/java/com/swipejam/backend/SwipeJamBackendApplication.java
@@ -0,0 +1,13 @@
+package com.swipejam.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwipeJamBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwipeJamBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/swipejam/backend/SwipeJamBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swipejam.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwipeJamBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
